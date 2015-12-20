1c45e72c2a2a84d7548945275663e18b24320fb3
bachkalo <a.bachkalo@gmail.com>
2015-12-20 03:25:29 +0200

    beautify the code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 for (var i = 0; i <= 100; i++) {
-		if ((i%3===0)&&(i%5===0)) {
-    	console.log("fizz"+"buzz" + " " + i);
-      }
-     else if (i%5===0) {
-    	console.log("buzz" + " " + i);
-    } else if (i%3===0) {
-    	console.log("fizz" + " " + i);
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log("fizz" + "buzz" + " " + i);
+    } else if (i % 5 === 0) {
+        console.log("buzz" + " " + i);
+    } else if (i % 3 === 0) {
+        console.log("fizz" + " " + i);
     }
-    if (!((i%3===0)||(i%5===0))) {
-    	console.log(i);
+    if (!((i % 3 === 0) || (i % 5 === 0))) {
+        console.log(i);
     }
 }
+fghft
